9427076b99c73d95393fdc8ca8694626c596f821
Oriondevcore <graham@oriondevcore.com>
2026-03-02 21:11:44 +0200

    uf

--- public/src/app/layout.tsx
+++ public/src/app/layout.tsx
@@ -71,22 +71,3 @@ export default function RootLayout({
     </html>
   );
 }
-import { Analytics } from "@vercel/analytics/next";
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  );
-}
